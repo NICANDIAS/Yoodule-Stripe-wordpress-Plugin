9a53a1673325023a9fc538266788e8aab082ef75
NICANDIAS <talk2liltig4life@gmail.com>
2022-08-01 23:17:20 +0100

    Added stripe library

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '0b20d75329b0a17a9ce33db3bc83dfd92350cc2b',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '0b20d75329b0a17a9ce33db3bc83dfd92350cc2b',
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v8.12.0',
+            'version' => '8.12.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'reference' => '1b7b1ea75c744b8a9f55a0942112f8e822a6a482',
+            'dev_requirement' => false,
+        ),
+    ),
+);
