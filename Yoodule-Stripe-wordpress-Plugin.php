d4035070a8bd2b83e47f8d3d5343bb224dec3ea8
NICANDIAS <talk2liltig4life@gmail.com>
2022-08-03 16:12:45 +0100

    merged shortcode to the plugin

--- Yoodule-Stripe-wordpress-Plugin.php
+++ Yoodule-Stripe-wordpress-Plugin.php
@@ -11,19 +11,21 @@
     function Yoodule_Stripe_menu(){
         add_menu_page( 'Test Plugin Page', 'Yoodule Stripe', 'manage_options', 'test-plugin', 'Stripe_api' );
     }
+
+    function yoodule_stripe_shortcode(){
+        return 'Follow us on <a rel="nofollow" href="https://twitter.com/Hostinger?s=20">Twitter</a>';
+    }
+    add_shortcode('yoodule_stripe', 'yoodule_stripe_shortcode');
     
     function Stripe_api(){
         echo "<h1>Yoodule Stripe api credentials</h1>";
         echo "<h2>Enter Credentials</h2>";
-?>
-        
-        <!-- Form to handle stripe Api -->
-        <form  action="secret-key.php" method="post" enctype="multipart/form-data">
-            Api key:  <input type="text" name="api" /><br />
-            Shortcode:  <input type="text" name="" /><br />
-            <?php submit_button('Submit') ?>
-        </form>
-    <?php
+
+            //Form to handle stripe Api
+        echo "<form  action='secret-key.php' method='post' enctype='multipart/form-data'>
+                Api key:  <input type='text' name='api' /><br />
+                <input type='Submit' name='Submit' />
+            </form>";
     }
 
 ?>
--- yoodule_stripe_shortcode.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function yoodule_stripe_shortcode(){
-return 'Follow us on <a rel="nofollow" href="https://twitter.com/Hostinger?s=20">Twitter</a>';
-}
-add_shortcode('yoodule_stripe', 'yoodule_stripe_shortcode');
-
-?>
