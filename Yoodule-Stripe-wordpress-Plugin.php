c451499b7bb5c447802930eb643cb2fa43d859c4
NICANDIAS <talk2liltig4life@gmail.com>
2022-08-03 21:34:20 +0100

    redering table from shortcode but not jQuery dataTables

--- Yoodule-Stripe-wordpress-Plugin.php
+++ Yoodule-Stripe-wordpress-Plugin.php
@@ -7,16 +7,40 @@
     */
     add_action('admin_menu', 'Yoodule_Stripe_menu');
 
-    
     function Yoodule_Stripe_menu(){
         add_menu_page( 'Test Plugin Page', 'Yoodule Stripe', 'manage_options', 'test-plugin', 'Stripe_api' );
     }
 
     function yoodule_stripe_shortcode(){
+        wp_enqueue_style('jquery-datatables-css','//cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css');
+        wp_enqueue_script('jquery-datatables-js','//cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js',array('jquery'));
+               
+        echo "<table id='example'>
+                <thead>
+                    <th>ID</th>
+                    <th>Title</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Testing1</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Testing2</td>
+                    </tr>
+                </tbody>
+                <thead>
+                    <th>ID</th>
+                    <th>Title</th>
+                </thead>
+            </table>";
+
         return 'Follow us on <a rel="nofollow" href="https://twitter.com/Hostinger?s=20">Twitter</a>';
     }
     add_shortcode('yoodule_stripe', 'yoodule_stripe_shortcode');
     
+    // Stripe Api Form
     function Stripe_api(){
         echo "<h1>Yoodule Stripe api credentials</h1>";
         echo "<h2>Enter Credentials</h2>";
